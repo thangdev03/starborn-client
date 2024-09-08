2901ef07cb3dff21b0a1e241a67d1b987d94eeb3
thangdev03 <nhatthangdev@gmail.com>
2024-09-08 11:36:15 +0700

    update AddProduct (unfinished)

--- src/pages/admin/AddProduct.jsx
+++ src/pages/admin/AddProduct.jsx
@@ -8,37 +8,44 @@ const AddProduct = () => {
   const [objectList, setObjectList] = useState([]);
   const [categoryList, setCategoryList] = useState([]);
   const [subcategoryList, setSubcategoryList] = useState([]);
+  const [selectedObject, setSelectedObject] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedSubcategory, setSelectedSubcategory] = useState(null);
+  const [variantNumber, setVariantNumber] = useState(1);
 
   const getObjects = () => {
     axios
-      .get(serverUrl + 'objects')
-      .then((res) => {
-        
-
-        setObjectList(res.data)
-      })
+      .get(serverUrl + 'categories/object')
+      .then((res) => setObjectList(res.data))
       .catch((err) => console.log(err))
   };
 
   const getCategories = () => {
     axios
-      .get(serverUrl + 'categories')
+      .get(serverUrl + 'categories?object_id=' + selectedObject?.id)
       .then((res) => setCategoryList(res.data))
       .catch((err) => console.log(err))
   };
 
   const getSubcategories = () => {
     axios
-      .get(serverUrl + 'subcategories')
+      .get(serverUrl + 'categories/subcategory?category_id=' + selectedCategory?.id)
       .then((res) => setSubcategoryList(res.data))
       .catch((err) => console.log(err))
   };
 
   useEffect(() => {
     getObjects();
-    getCategories();
-    getSubcategories();
   },[])
+
+  useEffect(() => {
+    getCategories();
+  },[selectedObject])
+
+  useEffect(() => {
+    getSubcategories();
+  },[selectedCategory])
+
 
   return (
     <Box sx={{ paddingX: {xs: '8px', md: '24px'}, margin: 0, paddingBottom: '160px' }}>
@@ -54,7 +61,7 @@ const AddProduct = () => {
             <AppBreadcrumbs />
         </Box>
 
-        <Stack gap={'20px'} sx={{ bgcolor: 'white', py: '40px', px: '32px', borderRadius: '16px' }}>
+        <Stack gap={'20px'} sx={{ mt: '24px', bgcolor: 'white', py: '40px', px: '32px', borderRadius: '16px' }}>
             <FormControl fullWidth variant="outlined" required>
                 <FormHelperText id="name-helper-text" sx={{ margin: '0 0 12px', fontSize: '16px', fontWeight: '500' }}>
                     Tên sản phẩm <span style={{color: colors.red}}>*</span>
@@ -86,66 +93,85 @@ const AddProduct = () => {
                     sx={{ paddingY: '8.5px' }}
                 />
             </FormControl>
+
             <div>
                 <Typography sx={{ fontSize: '16px', fontWeight: 500, mb: '16px' }}>
                     Phân loại <span style={{color: colors.red}}>*</span>
                 </Typography>
                 <Stack direction={{ xs: 'column', md: 'row' }} gap={'16px'}>
                     <Autocomplete
+                        id='select-object'
                         aria-required={'true'}
                         autoComplete={true}
-                        disablePortal
                         options={objectList}
-                        sx={{ flex: 1 }}
-                        renderInput={(params) => <TextField {...params} label="Đối tượng sử dụng" />}
+                        getOptionLabel={(option) => option?.name}
+                        sx={{ flex: 1, paddingY: '8.5px' }}
+                        size='small'
+                        value={selectedObject}
+                        onChange={(event, newValue) => {
+                            setSelectedObject(newValue);
+                            setSelectedCategory(null);
+                            setSelectedSubcategory(null);
+                        }}
+                        renderInput={(params) => <TextField {...params} label="Chọn đối tượng" />}
                     />
                     <Autocomplete
+                        id='select-category'
                         aria-required={'true'}
                         autoComplete={true}
-                        disablePortal
                         options={categoryList}
-                        sx={{ flex: 2 }}
-                        renderInput={(params) => <TextField {...params} label="Danh mục sản phẩm" />}
+                        getOptionLabel={(option) => option?.name}
+                        sx={{ flex: 1, paddingY: '8.5px' }}
+                        disabled={!selectedObject}
+                        size='small'
+                        value={selectedCategory}
+                        onChange={(event, newValue) => {
+                            setSelectedCategory(newValue);
+                            setSelectedSubcategory(null);
+                        }}
+                        renderInput={(params) => <TextField {...params} label="Chọn danh mục" />}
                     />
                     <Autocomplete
+                        id='select-subcategory'
                         aria-required={'true'}
                         autoComplete={true}
-                        disablePortal
                         options={subcategoryList}
-                        sx={{ flex: 2 }}
-                        renderInput={(params) => <TextField {...params} label="Tiểu danh mục sản phẩm" />}
+                        getOptionLabel={(option) => option?.name}
+                        sx={{ flex: 1, paddingY: '8.5px' }}
+                        disabled={!selectedCategory}
+                        size='small'
+                        value={selectedSubcategory}
+                        onChange={(event, newValue) => {
+                            setSelectedSubcategory(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} label="Chọn tiểu danh mục" />}
                     />
                 </Stack>
             </div>
+
             <Box>
                 <Divider>Các biến thể của sản phẩm</Divider>
-                <div style={{ margin: '16px 0' }}>
-                    <Stack direction={'row'} gap={'20px'} width={'100%'}>
-                        <Autocomplete
-                            aria-required={'true'}
-                            autoComplete={true}
-                            disablePortal
-                            options={['Nam', 'Nữ']}
-                            renderInput={(params) => <TextField {...params} label="Size" />}
-                        />
-                        <Autocomplete
-                            aria-required={'true'}
-                            autoComplete={true}
-                            disablePortal
-                            sx={{ flex: 1 }}
-                            options={['Nam', 'Nữ']}
-                            renderInput={(params) => <TextField {...params} label="Màu sắc" />}
-                        />
-                        <Autocomplete
-                            aria-required={'true'}
-                            autoComplete={true}
-                            disablePortal
-                            sx={{ flex: 1 }}
-                            options={['Nam', 'Nữ']}
-                            renderInput={(params) => <TextField {...params} label="Mã màu (hex)" />}
-                        />
-                    </Stack>
-                </div>
+                <Stack gap={'20px'} style={{ margin: '16px 0' }}>
+                    <Box>
+                        <Stack direction={'row'} gap={'20px'} width={'100%'}>
+                            <Autocomplete
+                                id='variant-size-1'
+                                aria-required={true}
+                                autoComplete={true}
+                                disablePortal
+                                size='small'
+                                options={['3XL', '2XL', 'XL', 'L', 'M', 'SM', 'XS']}
+                                renderInput={(params) => <TextField {...params} label="Size" />}
+                            />
+                            <TextField id="variant-color" label="Màu sắc" variant="outlined" aria-required size='small'/>
+                            <TextField id="variant-hex" label="Mã màu (hex)" variant="outlined" aria-required size='small' type='color' sx={{ width: '120px' }}/>
+                            <TextField id="variant-price" label="Giá (VNĐ)" variant="outlined" aria-required size='small' type='number'/>
+                            <TextField id="variant-discount" label="Giảm giá (%)" variant="outlined" aria-required size='small' type='number'/>
+                            <TextField id="variant-quantity" label="Số lượng tồn kho" variant="outlined" aria-required size='small' type='number'/>
+                        </Stack>
+                        <>Tair anh len</>
+                    </Box>
+                </Stack>
             </Box>
         </Stack>
     </Box>
